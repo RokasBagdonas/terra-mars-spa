64d06c419e2d416dce852c0f2a97d52899069cf5
RokasBagdonas <rokius.bagdonas@gmail.com>
2021-07-15 08:57:12 +0100

    chore: setup fontawesome icons

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,28 @@
 // Base
 import camelCase from "lodash/camelCase";
+
 // Utilities
 import upperFirst from "lodash/upperFirst";
 import {createApp} from 'vue';
 import App from './App.vue';
+
 // Created
 import "./css/main.scss";
 import router from './router';
+
 // Other
 import AuthService from "./auth/AuthService";
 
+//fontawesome
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {faTimes, faAngleDown} from '@fortawesome/free-solid-svg-icons'
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
+
+
 const app = createApp(App);
+
+library.add(faAngleDown, faTimes);
+app.component("font-awesome-icon", FontAwesomeIcon);
 
 // ============================================================================
 const requireComponent = require.context(
