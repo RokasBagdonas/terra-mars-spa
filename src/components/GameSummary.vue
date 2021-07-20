1a8bde97277885743039aec1efe4b0c143c6ae79
RokasBagdonas <rokius.bagdonas@gmail.com>
2021-07-20 13:38:26 +0100

    styling: GameSummary title colours

--- src/components/GameSummary.vue
+++ src/components/GameSummary.vue
@@ -2,18 +2,30 @@
   <div class="card">
     <header class="card-header">
       <p class="card-header-title is-clickable" @click="toggleDisplayDetails">
-        {{ Game.pretifyDate(gs.date) }}. Generations:
-        {{ gs.number_of_generations }} winner: {{ GameScoresClass.getWinner(gs) }}
+        <span class="has-text-grey-dark">{{ Game.pretifyDate(gs.date) }} </span>
+        <span class="mx-1 has-text-grey">
+          Generations: {{ gs.number_of_generations }}
+        </span>
+        <span class="has-text-grey">
+          winner: {{ GameScoresClass.getWinner(gs) }}
+        </span>
       </p>
       <div class="level-right">
-        <span class="level-item icon is-clickable" @click="toggleDisplayDetails">
-          <font-awesome-icon v-if="!displayDetails" class="fas fa-angle-down" icon="angle-down" />
+        <span
+          class="level-item icon is-clickable"
+          @click="toggleDisplayDetails"
+        >
+          <font-awesome-icon
+            v-if="!displayDetails"
+            class="fas fa-angle-down"
+            icon="angle-down"
+          />
           <font-awesome-icon v-else class="fas fa-times" icon="times" />
         </span>
       </div>
     </header>
     <div v-if="displayDetails" class="card-content">
-      <GameScores :gameScores="gs"/>
+      <GameScores :gameScores="gs" />
     </div>
   </div>
 </template>
@@ -32,17 +44,18 @@ export default {
   setup(props) {
     let gs = new GameScoresClass(props.gameScores);
     let displayDetails = ref(false);
-    const toggleDisplayDetails = () => displayDetails.value = !displayDetails.value;
+    const toggleDisplayDetails = () =>
+      (displayDetails.value = !displayDetails.value);
     return {
       gs,
       GameScoresClass,
       Game,
       toggleDisplayDetails,
-      displayDetails
+      displayDetails,
     };
   },
   components: {
     GameScores,
-  }
+  },
 };
 </script>
