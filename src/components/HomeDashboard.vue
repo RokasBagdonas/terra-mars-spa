41d6814808b3c36a3402ca56e745883cae65b46f
RokasBagdonas <rokius.bagdonas@gmail.com>
2021-07-20 13:26:12 +0100

    feat: Add banner description

--- src/components/HomeDashboard.vue
+++ src/components/HomeDashboard.vue
@@ -1,4 +1,33 @@
 <template>
+  <section class="hero">
+    <div class="hero-body box content">
+      <h3>Website for recording and tracking your
+      <a
+        class="link has-text-primary"
+        href="https://www.fryxgames.se/games/terraforming-mars/"
+        >Terraforming Mars</a
+      >
+      games!
+      </h3>
+      How to use it:
+      <ul>
+        <li>Register as a player in Add Player (nickname has to be unique!)</li>
+        <li>
+          Go to Add Game, enter game details, number of players press confirm.
+        </li>
+        <li>
+          Enter player scores. Once finished, scroll down and press
+          <span class="tag">Add Game</span>
+        </li>
+        <li>The game will now appear in the HomeDashboard or in Games.</li>
+      </ul>
+      <h6>Player Statistics</h6>
+      <p class="content">
+        Every time a game is added, you can Update your statistics in Player
+        Stats section.
+      </p>
+    </div>
+  </section>
   <div class="section">
     <div class="columns">
       <div class="column is-one-third is-narrow" style="overflow-x: scroll">
